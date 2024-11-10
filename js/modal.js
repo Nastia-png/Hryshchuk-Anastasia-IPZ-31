2520d5358bd0acb25bd8d60f301d4a13ae246316
Nastia <grisuknasta278@gmail.com>
2024-11-10 17:30:23 +0200

    fix modal window

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,16 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+    const refs = {
+        openModalBtn: document.querySelector(".btn-order-service"),
+        // menuBtn: document.querySelector(".menu-btn"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+        // modalSecond: document.querySelector("[data-modal-second]"),
+    };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
 })();
